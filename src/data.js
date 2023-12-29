45de7bee1b2178831fc5193b25497e8acf3b4cce
sofiahag <sofia.h4glund@gmail.com>
2023-12-29 15:25:03 +0100

    minor fix

--- src/data.js
+++ src/data.js
@@ -16,7 +16,7 @@ export const headerData = {
             href: 'https://github.com/sofiahag/sofiahag-react-todo'
           },
           {
-            text: 'Vanilla ES6 user app',
+            text: 'Vanilla ES6 user App',
             href: 'https://github.com/sofiahag/user_app'
           }
         ]
